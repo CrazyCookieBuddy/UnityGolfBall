9b8cbf6e7c44a60d72665583482982ad145f1fa5
simon <stockfisch97@gmail.com>
2021-12-09 15:38:23 +0100

    Initial Commit Electric Boogaloo
    Added all the code etc

---
+++ Assets/Scripts/SC_Ball.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class SC_Ball : MonoBehaviour
+{
+
+    SC_TrajectoryController TC;
+    Rigidbody rb;
+    public SC_TrajectoryController PManager;
+    int stoppingCounter;
+    bool isActivePlayer = false;
+    public bool isDummy = false;
+    public bool hasHit = false;
+    public Vector3 v = new Vector3(1, 1, 0);
+
+    public float maxForce = 2000;
+    float forceIncrementSize = 250;
+    public float force;
+    public bool useMaxForce = false;
+    float aimRotation = 0;
+    public float stepSize = 2;
+    public float radius;
+    public bool canShoot;
+    bool startOfRoll = false;
+    float stopBelowVelo = 0.136f;
+    float initialShotWaitTime = 0.25f;
+
+
+    float MoveAxis;
+
+    public bool leftGround;
+    public Modes mode = Modes.Short;
+    public LayerMask layer;
+
+    public float overlapResize = 1.4f;
+
+
+    public enum Modes
+    {
+        Short,
+        Long,
+        Jump
+    }
+
+    private void Awake()
+    {
+        layer = 1 << 8;
+        layer = ~layer;
+        if (TC == null)
+        {
+            TC = GetComponent<SC_TrajectoryController>();
+        }
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+
+        }
+        if (PManager == null)
+        {
+            GameObject d = GameObject.Find("PredictionManager");
+            PManager = d.GetComponent<SC_TrajectoryController>();
+        }
+    }
+
+    public void Update()
+    {
+        if (!isActivePlayer)
+        {
+            PauseBall();
+        }
+        if (!canShoot && !startOfRoll)
+        {
+            if (rb.velocity.magnitude > stopBelowVelo)
+            {
+                //still rolling
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+                rb.velocity = Vector3.zero;
+                canShoot = true;
+            }
+        }
+    }
+
+    public void PauseBall()
+    {
+        
+    }
+    public bool isInAir()
+    {
+        bool b = false;
+        //Collider[] hitCollider = Physics.OverlapSphere(rb.position, rb.transform.localScale.x * overlapResize, layer);
+        float r = rb.transform.localScale.x / 2f;
+        Collider[] hitCollider = Physics.OverlapBox(rb.position, new Vector3(r, overlapResize, r), Quaternion.identity, layer);
+        if (hitCollider.Length > 0)
+        {
+            b = true;
+        }
+        return !b;
+
+    }
+    public void DebugShoot(InputAction.CallbackContext context)
+    {
+
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            if (canShoot)
+            {
+                startOfRoll = true;
+                StartCoroutine(initialShotWait(initialShotWaitTime));
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(v);
+                PManager.ClearPath();
+                canShoot = false;
+            }
+        }
+    }
+    public void RotateAim(InputAction.CallbackContext context)
+    {
+        MoveAxis = context.ReadValue<float>();
+
+        if (context.phase == InputActionPhase.Started)
+        {
+            if (MoveAxis != 0)
+            {
+
+                Rotate(MoveAxis, stepSize);
+                setV(force);
+            }
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+        }
+    }
+    void clampForce()
+    {
+        force = Mathf.Clamp(force, 0, maxForce);
+    }
+    public void ModeSwitch(InputAction.CallbackContext context)
+    {
+
+        if (context.phase == InputActionPhase.Started)
+        {
+            mode = (Modes)((int)(mode + 1) % System.Enum.GetNames(typeof(Modes)).Length);
+            setV(force);
+            DrawPreview();
+        }
+    }
+    void Rotate(float dir, float deg)
+    {
+        aimRotation += deg * dir;
+        setV(force);
+        DrawPreview();
+
+    }
+    public void Force(InputAction.CallbackContext context)
+    {
+        float MoveAxis = context.ReadValue<float>();
+        if (context.phase == InputActionPhase.Started)
+        {
+            force += forceIncrementSize * MoveAxis;
+            clampForce();
+            setV(force);
+            DrawPreview();
+        }
+    }
+    void DrawPreview()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        float x = Mathf.Cos(aimRotation * Mathf.Deg2Rad) * (useMaxForce == true ? maxForce : force);
+        float z = Mathf.Sin(aimRotation * Mathf.Deg2Rad) * (useMaxForce == true ? maxForce : force);
+        PManager.Predict(gameObject, gameObject.transform.position, new Vector3(x, mode == Modes.Jump ? (useMaxForce == true ? maxForce : force) : 0, z));
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+
+    }
+    //void DrawPreview(Vector3 v2)
+    //{
+    //    rb.constraints = RigidbodyConstraints.None;
+    //    PManager.Predict(gameObject, gameObject.transform.position, v2);
+    //    rb.constraints = RigidbodyConstraints.FreezeAll;
+    //}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float d = Vector3.Dot(collision.contacts[0].normal, Vector3.up);
+
+        if (mode != Modes.Jump)
+        {
+            if (leftGround)
+            {
+                PManager.dummyCollisions = PManager.maxCollisions;
+                return;
+            }
+        }
+
+
+        switch (d)
+        {
+            case 1.0f:
+                //floor
+                break;
+            case 0.0f:
+                //wall
+                break;
+            default:
+                //else
+                break;
+        }
+        if (isDummy && !hasHit && leftGround)
+        {
+            hasHit = true;
+            PManager.dummyCollisions++;
+            return;
+        }
+    }
+
+    void setV(float f)
+    {
+        float x = Mathf.Cos(aimRotation * Mathf.Deg2Rad) * f;
+        float z = Mathf.Sin(aimRotation * Mathf.Deg2Rad) * f;
+        v = new Vector3(x, mode == Modes.Jump ? f : 0, z);
+    }
+    IEnumerator initialShotWait(float t)
+    {
+        startOfRoll = true;
+        yield return new WaitForSeconds(t);
+        startOfRoll = false;
+    }
+}
---
+++ Assets/Scripts/SC_TrajectoryController.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_TrajectoryController : MonoBehaviour
+{
+    public List<GameObject> obstacles;
+    public int maxIterations;
+
+    Scene currentScene;
+    Scene predictionScene;
+
+    PhysicsScene currentPhysicsScene;
+    PhysicsScene predictionPhysicsScene;
+
+    List<GameObject> dummyObstacles = new List<GameObject>();
+
+    public LineRenderer lineRenderer;
+
+    public List<Vector3> positions;
+
+
+    GameObject dummy;
+    public int dummyCollisions = 0;
+    public int maxCollisions;
+    public float[] bounces = new float[3];
+    public float distanceTraveled = 0f;
+    List<GameObject> balls;
+    public GameObject TrajectoryPrefab;
+    public float ballSpacing = 0.5f;
+
+
+    private void Awake()
+    {
+        positions = new List<Vector3>();
+        balls = new List<GameObject>();
+        bounces = new float[maxCollisions];
+        obstacles.Clear();
+        foreach (Collider c in GameObject.FindObjectsOfType<Collider>())
+        {
+            if (c.gameObject != null)
+            {
+                if (c.CompareTag("Obstacle"))
+                {
+                    obstacles.Add(c.gameObject);
+                }
+            }
+        }
+        Physics.autoSimulation = false;
+
+        currentScene = SceneManager.GetActiveScene();
+        currentPhysicsScene = currentScene.GetPhysicsScene();
+
+        CreateSceneParameters parameters = new CreateSceneParameters(LocalPhysicsMode.Physics3D);
+        predictionScene = SceneManager.CreateScene("Prediction", parameters);
+        predictionPhysicsScene = predictionScene.GetPhysicsScene();
+
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (currentPhysicsScene.IsValid())
+        {
+            currentPhysicsScene.Simulate(Time.fixedDeltaTime);
+        }
+    }
+    private void Update()
+    {
+        float width = lineRenderer.startWidth;
+        lineRenderer.material.mainTextureScale = new Vector2(1f / width, 1.0f);
+    }
+
+    public void copyAllObstacles()
+    {
+        foreach (GameObject o in obstacles)
+        {
+            if (o.gameObject.GetComponent<Collider>() != null)
+            {
+                GameObject fakeT = Instantiate(o.gameObject);
+                fakeT.transform.position = o.transform.position;
+                fakeT.transform.rotation = o.transform.rotation;
+                Renderer fakeR = fakeT.GetComponent<Renderer>();
+                if (fakeR)
+                {
+                    fakeR.enabled = false;
+                }
+                SceneManager.MoveGameObjectToScene(fakeT, predictionScene);
+                dummyObstacles.Add(fakeT);
+            }
+
+        }
+    }
+
+    void killAllObstacles()
+    {
+        foreach (var o in dummyObstacles)
+        {
+            Destroy(o);
+        }
+        dummyObstacles.Clear();
+    }
+
+    public void Predict(GameObject subject, Vector3 currentPosition, Vector3 force)
+    {
+        ClearPath();
+        positions.Clear();
+
+        dummyCollisions = 0;
+        if (currentPhysicsScene.IsValid() && predictionPhysicsScene.IsValid())
+        {
+            if (dummy == null)
+            {
+                dummy = Instantiate(subject);
+                SceneManager.MoveGameObjectToScene(dummy, predictionScene);
+            }
+            copyAllObstacles();
+            dummy.GetComponent<SC_Ball>().isDummy = true;
+            dummy.transform.position = currentPosition;
+            dummy.GetComponent<Rigidbody>().AddForce(force);
+            lineRenderer.positionCount = maxIterations;
+
+            int t_dummyCollision = dummyCollisions;
+            float distance = 0f;
+            for (int i = 0; i < maxIterations; i++)
+            {
+
+                if (dummy.GetComponent<SC_Ball>().leftGround == false)
+                {
+                    dummy.GetComponent<SC_Ball>().leftGround = dummy.GetComponent<SC_Ball>().isInAir();
+                }
+
+
+
+                predictionPhysicsScene.Simulate(Time.fixedDeltaTime);
+
+                positions.Add(dummy.transform.position - new Vector3(0, dummy.transform.localScale.y / 1.2f, 0));
+                lineRenderer.SetPosition(i, dummy.transform.position - new Vector3(0, dummy.transform.localScale.y / 1.2f, 0));
+
+                if (i != 0)
+                {
+                    distance += (positions[i - 1] - positions[i]).magnitude;
+                }
+
+
+                if (dummy.GetComponent<SC_Ball>().hasHit)
+                {
+                    bounces[dummyCollisions - 1] = distance;
+                }
+
+                dummy.GetComponent<SC_Ball>().hasHit = false;
+
+                if (dummyCollisions >= maxCollisions)
+                {
+                    lineRenderer.positionCount = i;
+                    break;
+                }
+            }
+            distanceTraveled = distance;
+            SetLineColors();
+            MakePathBalls();
+            Destroy(dummy);
+        }
+        killAllObstacles();
+        SetPathBallMaterial();
+
+
+
+    }
+
+    [ContextMenu("Balls")]
+    void MakePathBalls()
+    {
+        float spacing = 0f;
+        for (int i = 0; i < positions.Count; i++)
+        {
+            if (i != 0)
+            {
+                spacing += (positions[i - 1] - positions[i]).magnitude;
+                if (spacing >= ballSpacing)
+                {
+                    balls.Add(Instantiate(TrajectoryPrefab, positions[i], Quaternion.identity));
+                    spacing = 0f;
+                }
+            }
+
+        }
+    }
+    void SetPathBallMaterial()
+    {
+        //add color or transparency
+        float f = balls.Count;
+
+        int i = 0;
+        foreach (GameObject item in balls)
+        {
+            i++;
+            Color c = item.gameObject.GetComponent<MeshRenderer>().material.color;
+            float t =((f+(f/0.3f)) / (float)i)/f;
+            Debug.Log(t);
+            c.a = t;
+            item.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", c);
+
+        }
+
+    }
+    void ClearPathBalls()
+    {
+        foreach (GameObject b in balls)
+        {
+            Destroy(b);
+        }
+        balls.Clear();
+    }
+
+
+    void SetLineColors()
+    {
+
+    }
+    void OnDestroy()
+    {
+        killAllObstacles();
+    }
+
+    public void ClearPath()
+    {
+        ClearPathBalls();
+        lineRenderer.positionCount = 0;
+    }
+}
